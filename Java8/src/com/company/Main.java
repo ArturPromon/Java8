c5efafbf75f1f06882c0c93fcaca551074aaab63
ArturPromon <artur.shovkunenko@gmail.com>
2023-05-03 19:02:54 +0300

    Data types

---
+++ Java8/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        //int это целые числа
+        int number = 10;
+        System.out.println(number);
+        //float double числа с плавающей запятой
+        float number2 = 10.5f;
+        System.out.println(number2);
+        double number3 = 10.8d;
+        System.out.println(number3);
+        // [-128, 128)
+        byte number4 = 127;
+        // [-32,768 to 32,768)
+        short number5 = 2000;
+        long number6 = 1241241241;
+        char sym = '+';
+        String str = "Hello world";
+        //boolean true or false
+        boolean isTrue = true;
+        System.out.println(isTrue);
+    }
+}

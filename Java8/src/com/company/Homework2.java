7d6530996f035459cfd1a4140c94aa11ca883530
ArturPromon <artur.shovkunenko@gmail.com>
2023-05-26 18:52:37 +0300

    Homeworks

--- Java8/src/com/company/Homework2.java
+++ Java8/src/com/company/Homework2.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Homework2 {
     public static void main(String[] args) {
-       String[] array = {"K", "O", "Z", "Z", "G", "S", "F"};
+        String[] array = {"K", "O", "Z", "Z", "G", "S", "F"};
 //        OUT ["F", "S", "G", "Z", "Z", "O", "K"]
+        String temp;
+        for(int i = 0; i< array.length/2;i++){
+            temp = array[array.length-i-1];
+            array[array.length-i-1] = array[i];
+            array[i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
---
+++ Java8/src/com/company/example.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class example {
+        public static void main(String[] args) {
+            String[] array = {"K", "O", "Z", "Z", "G", "S", "F"};
+            //OUT ["F", "S"´,"G", "Z", "Z", "O",  "K"]
+            // BEZ VSPOMOGATELNOGO MASSIVA
+            System.out.println(Arrays.toString(array));
+//            for (int s = array.length - 1; s > 0 ; s--) {
+//                System.out.print(array[s] +", ");
+//            }
+            String[] array2 = new String[array.length];
+            int index = 0;
+            for (int i = array.length-1;i >= 0; i--){
+                array2[index] = array[i];
+                index++;
+            }
+            System.out.println(Arrays.toString(array2));
+
+    }
+}

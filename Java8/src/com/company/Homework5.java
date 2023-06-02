047c28b7c315ff1904eaa6a22b1b25bb282109de
ArturPromon <artur.shovkunenko@gmail.com>
2023-06-02 17:37:50 +0300

    homework5

--- Java8/src/com/company/Homework5.java
+++ Java8/src/com/company/Homework5.java
@@ -1,8 +1,17 @@
 package com.company;
-
 public class Homework5 {
     public static void main(String[] args) {
-        String str = "Hello Happy new year";
+        String str = "Hello   gtg  fge  ";
         //Вывести индекс последней буквы
+        int length = str.length();
+        int index = length - 1;
+        while(true){
+            if(str.charAt(index) == ' '){
+                index--;
+            }else{
+                System.out.println(index);
+                break;
+            }
+        }
     }
 }
